6c2fe2c902a9ba6e879812c3494a112d7d6cfd79
Saurabh Panwar <sp185432@ncr.com>
2019-06-19 06:16:09 +0530

    modifying

--- add_main.go
+++ add_main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/sp185432/my_sample_go/addition"
 )
 
 func main() {
 	fmt.Println("This is a sample test")
-	fmt.Println("This is addition of 7 and 8: ", addition.Add(7,8))
+	fmt.Println("This is addition of 7 and 8: ", addition.Add(7, 8))
 }
