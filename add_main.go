a24f46d630792dc4fc933419de07d9bbbe654187
Saurabh Panwar <sp185432@ncr.com>
2019-06-19 05:42:13 +0530

    Modifying main to use Add functionality

--- add_main.go
+++ add_main.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	fmt.Println("This is a sample test")
-	fmt.Println("This is addition of 7 and 8: ", Add(7,8))
+	fmt.Println("This is addition of 7 and 8: ", addition.Add(7,8))
 }
